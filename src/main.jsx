2a8c8f9b5d8e552744c73d8bd94afb66201de998
rchowd9 <chowdhuryriasat078@gmail.com>
2026-07-25 21:46:28 -0400

    Fixed the issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 
 // Mount React app to the #root div in index.html
 ReactDOM.createRoot(document.getElementById("root")).render(
