18de0c0486370241ffacf7f2bf489e817657439c
rchowd9 <chowdhuryriasat078@gmail.com>
2026-07-27 20:40:58 -0400

    Adjusted the vite section

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/rchowd9/personal-portfolio', // <-- Replace 'your-repo-name' with your GitHub repo name
   build: {
-    outDir: 'dist',   // Vercel expects your build output here
+    outDir: 'dist',
   },
   server: {
-    port: 3000,       // Local dev server port
-    open: true        // Auto-open browser on npm run dev
-  }
+    port: 3000,
+    open: true,
+  },
 });
