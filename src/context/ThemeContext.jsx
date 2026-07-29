dbd84cb5fd7c768865942e2c973d372537c3a773
rchowd9 <chowdhuryriasat078@gmail.com>
2026-07-29 19:33:03 -0400

    added toggle button

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,15 +1,16 @@
+// src/context/ThemeContext.jsx
 import React, { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  
+  // Check local storage or fallback to dark mode default
   const [isDark, setIsDark] = useState(() => {
     const saved = localStorage.getItem("theme");
     return saved ? saved === "dark" : true;
   });
 
-    useEffect(() => {
+  useEffect(() => {
     const theme = isDark ? "dark" : "light";
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
