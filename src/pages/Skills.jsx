0077d167a15c624e71e84f2e1ed341af810048c6
rchowd9 <chowdhuryriasat078@gmail.com>
2026-08-11 12:13:27 -0400

    added certificate

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -117,6 +117,7 @@ const skillCategories = [
 const certifications = [
   "Google Cloud Fundamentals (Jul 2025)",
   "Intro to Technical Interview Prep (Apr 2026)",
+  "Foundations of AI Engineering (Aug 2026)",
 ];
 
 export default function Skills() {
