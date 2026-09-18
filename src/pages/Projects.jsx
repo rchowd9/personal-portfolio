c3386da17967103ae27dc7f873d0c9f7589ce99a
rchowd9 <chowdhuryriasat078@gmail.com>
2026-09-17 21:01:13 -0400

    added different liveurl

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -56,7 +56,7 @@ export default function Projects() {
       description:
         "A fast-paced 1v1 HTML5 fighting game built around a high-stakes 'one-hit KO' dash mechanic. Designed as a software engineering class project, it implements a state machine managing 'SLIPSTREAM', 'RECOVERY', and 'NEUTRAL' states to simulate precise frame-dependent combat, collision detection, and draw conditions.",
       tech: ["HTML", "CSS", "JavaScript"],
-      liveUrl: "https://rchowd9.github.io/Slipstream/",
+      liveUrl: "https://slipstream-iota-five.vercel.app/",
       repoUrl: "https://github.com/rchowd9/Slipstream",
     },
 
